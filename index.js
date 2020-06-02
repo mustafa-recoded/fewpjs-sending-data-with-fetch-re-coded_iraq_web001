115ea22763cf873b32232584940ae2941775ea73
Mustafa Nawzat <mustafa@re-coded.com>
2020-06-02 20:37:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(username,useremail){
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: username,
+        email: useremail
+      })
+    }
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function (object) {
+        document.body.innerHTML = object[ "id" ]
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error.message
+    })
+    
+}
